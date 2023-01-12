ce040dc9e24834147a2bf6cb3ac4353c4107a595
vp-en <67713433+vp-en@users.noreply.github.com>
2023-01-12 17:55:30 +0100

    Initialize release

---
+++ update-versions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+branch="master"
+version=$(curl -fsSL "https://prowlarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -r .[0].version)
+[[ -z ${version} ]] && exit 0
+[[ ${version} == "null" ]] && exit 0
+version_json=$(cat ./VERSION.json)
+jq '.version = "'"${version}"'" | .sbranch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
